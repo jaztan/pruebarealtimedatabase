338a06c9e82ed292415d9c9db689c1704882a22e
jaztan <jaztangr@outlook.com>
2018-07-24 10:45:04 -0500

    prueba usando realtimedatabase

---
+++ app.js
@@ -0,0 +1,13 @@
+//login
+// Initialize Firebase
+//codigo obtenido de la pagina de firebas del proyecto
+var config = {
+    apiKey: "AIzaSyCm7I3cvutJG8L4Sbt7BiK-VQdPdxk3i4Y",
+    authDomain: "count-on-me-476fd.firebaseapp.com",
+    databaseURL: "https://count-on-me-476fd.firebaseio.com",
+
+    projectId: "count-on-me-476fd",
+    storageBucket: "count-on-me-476fd.appspot.com",
+    messagingSenderId: "578346489088"
+  };
+  firebase.initializeApp(config);
